b16d246ef855d055e0c4ea5a62cb5559254016e7
PolztiukVolodymyr <92870556+PolztiukVolodymyr@users.noreply.github.com>
2022-07-14 16:40:11 +0300

    fix json

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,6 +3,7 @@ import css from './App.module.css';
 import ContactForm from "../ContactForm/ContactForm";
 import ContactList from "../ContactList/ContactList";
 import Filter from "../Filter/Filter"
+import { toHaveDisplayValue } from "@testing-library/jest-dom/dist/matchers";
 
 export class App extends Component {
   state = {
@@ -14,6 +15,13 @@ export class App extends Component {
     ],
     filter: '',
   };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState) {
+      console.log("Change!")
+      localStorage.setItem("contacts", JSON.stringify(this.state.contacts))
+    }
+  }
 
   onSubmit = data => {
     const contactNames = this.state.contacts.map(contact => contact.name);
